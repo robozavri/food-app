13f82c41d13ba62e915f61edba4b86dae23227a6
robozavri <nika37912@hotmail.com>
2021-06-10 14:12:13 +0400

    [feat][lesson 171]: create folder structure screens and widgets

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-import 'category_meals_screen.dart';
+import '../screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
